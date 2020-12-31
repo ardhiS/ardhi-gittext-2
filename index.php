4b2b914f0d368913835ff095548ac8c916daa730
Ardhi Sasongko <Ardhi333@gmail.com>
2020-12-31 19:05:58 +0700

    Menambahkan file index.php]

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ardhi sasongko</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ea, error nihil dicta illum aliquam aspernatur repellat a ab optio quae. Autem placeat esse, doloribus deserunt ipsam magnam repellendus eum?</p>
+</body>
+</html>
