4b4a04a7e39393dd82bfc2b668fa13b4a52217aa
Ardhi Sasongko <Ardhi333@gmail.com>
2020-12-31 19:13:43 +0700

    Menambahkan paragrap baru

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Ardhi sasongko</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ea, error nihil dicta illum aliquam aspernatur repellat a ab optio quae. Autem placeat esse, doloribus deserunt ipsam magnam repellendus eum?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque enim magnam iure iste unde alias ut molestiae commodi exercitationem? Ex illum voluptatem quis, quaerat veniam id iste non culpa architecto!</p>
 </body>
 </html>
